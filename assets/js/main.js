a659fa3b20c8009b784d2bc461c38757eb5708af
Alexander Hg <alexanderhuamanguevara@gmail.com>
2022-04-24 16:38:14 -0500

    casi_final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
 /*=============== SHOW SIDEBAR ===============*/
-
+const navMenu=document.getElementById('sidebar'),
+    navToggle=document.getElementById('nav-toggle'),
+    navClose=document.getElementById('nav-close')
 
 /*===== SIDEBAR SHOW =====*/
 /* Validate If Constant Exists */
-
+if(navToggle){
+    navToggle.addEventListener("click",()=>{
+        navMenu.classList.add('show-sidebar')
+    })
+}
 
 /*===== SIDEBAR HIDDEN =====*/
 /* Validate If Constant Exists */
-
+if(navToggle){
+    navClose.addEventListener("click",()=>{
+        navMenu.classList.remove('show-sidebar')
+    })
+}
 
 /*=============== SKILLS TABS ===============*/
 const tabs=document.querySelectorAll('[data-target]'),
@@ -100,9 +110,47 @@ modalCloses.forEach((modalclose)=>{
 
 
 /*=============== INPUT ANIMATION ===============*/
+const inputs= document.querySelectorAll(".input");
 
+function focusfunc(){
+    let parent=this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurfunc(){
+    let parent=this.parentNode;
+    if(this.value==""){
+        parent.classList.remove("focus")
+    }
+
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener("focus",focusfunc);
+    input.addEventListener("blur",blurfunc)
+})
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+ const sections=document.querySelectorAll("section[id]");
 
+window.addEventListener("scroll",navHighligther);
+
+ function navHighligther(){
+     let scrollY=window.pageYOffset;
+
+     sections.forEach(current=>{
+         const sectionHeigth=current.offsetHeight;
+         const sectionTop=current.offsetTop -50,
+         sectionId=current.getAttribute("id");
+
+     if(scrollY>sectionTop && scrollY <=sectionTop+sectionHeigth)
+     {
+         document.querySelector('.nav__menu a[href*='+sectionId+"]").classList.add("active-link")
+     }   
+     else{
+        document.querySelector('.nav__menu a[href*='+sectionId+"]").classList.remove("active-link")
+     }
+     })
+ }
 
 /*=============== SHOW SCROLL UP ===============*/
